21e11ed588095f090add06d4d97a83ddcdfd9290
Paulina  Kosińska <paulinakosinska95@gmail.com>
2018-04-02 20:57:04 +0200

    add tests for ul elements

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,21 @@
-QUnit.test( "hello test", function( assert ) {
-    assert.ok( 1 == "1", "Passed!" );
-  });
+QUnit.test("li fadeIn test", function(assert) {
+  var done = assert.async();
+  $('#test1Ul').hide().animateChildrenOneByOne('fadeIn', 1, 1);
+  setTimeout (function() {
+    var isVisible = $('#test1Ul > li').is(":visible");
+    assert.ok( isVisible , "passed");
+    done();
+ }, 15 );
+});
+
+
+QUnit.test("li fadeOut test", function(assert) {
+  var done = assert.async();
+  $('#test2Ul').animateChildrenOneByOne('fadeOut', 1, 1);
+  setTimeout (function() {
+    var isVisible = $('#test2Ul > li').is(":visible");
+    var isNotVisible = !isVisible;
+    assert.ok( isNotVisible , "passed");
+    done();
+  }, 15 );
+});
