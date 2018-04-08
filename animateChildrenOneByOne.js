e5a4b23c215bb59d11a442e81ddae9a743da447c
Paulina  Kosińska <paulinakosinska95@gmail.com>
2018-04-08 21:31:35 +0200

    iteration over every catched element

--- animateChildrenOneByOne.js
+++ animateChildrenOneByOne.js
@@ -2,31 +2,34 @@ $.fn.animateChildrenOneByOne = function (animate, delay, duration) {
 	delay = (delay !== undefined) ? delay : 2000;
 	duration = (duration !== undefined) ? duration : 500;
 
-	var i = 0;
-	this.children().each(function () {
-		var currentElement = $(this);
-		i++;
-		setTimeout(function() {
-			switch (animate) {
-			case 'fadeIn':
-				currentElement.fadeIn(duration);
-				break;
-			case 'fadeOut':
-				currentElement.fadeOut(duration);
-				break;
-			case 'slideDown':
-				currentElement.slideDown(duration);
-				break;
-			case 'slideUp':
-				currentElement.slideUp(duration);
-				break;
-			case 'hide':
-				currentElement.hide(duration);
-				break;
-			default:
-				currentElement.show(duration);
-				break;
-			}
-		}, i * delay);
-	});
+	this.each(function() {
+		var i = 0;
+		$(this).children().each(function () {
+			var currentElement = $(this);
+			i++;
+			setTimeout(function() {
+				switch (animate) {
+				case 'fadeIn':
+					currentElement.fadeIn(duration);
+					break;
+				case 'fadeOut':
+					currentElement.fadeOut(duration);
+					break;
+				case 'slideDown':
+					currentElement.slideDown(duration);
+					break;
+				case 'slideUp':
+					currentElement.slideUp(duration);
+					break;
+				case 'hide':
+					currentElement.hide(duration);
+					break;
+				default:
+					currentElement.show(duration);
+					break;
+				}
+			}, i * delay);
+		});
+	})
+	
 };
