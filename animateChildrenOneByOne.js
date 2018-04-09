b3cb6bfd31b9e86000a2366abebcf5c564c55c4b
Paulina  Kosińska <paulinakosinska95@gmail.com>
2018-04-09 23:04:58 +0200

    add console error

--- animateChildrenOneByOne.js
+++ animateChildrenOneByOne.js
@@ -1,7 +1,7 @@
 $.fn.animateChildrenOneByOne = function (animate, delay, duration) {
 	delay = (delay !== undefined) ? delay : 2000;
 	duration = (duration !== undefined) ? duration : 500;
-
+	
 	this.each(function () {
 		var i = 0;
 		$(this).children().each(function () {
@@ -11,7 +11,7 @@ $.fn.animateChildrenOneByOne = function (animate, delay, duration) {
 				try {
 					currentElement[animate](duration);
 				} catch (err) {
-					console.error(`Invalid animation [${err}]`);
+					console.error(`Invalid animation name: ${animate} [${err}]`);
 				}
 			}, i * delay);
 		});
