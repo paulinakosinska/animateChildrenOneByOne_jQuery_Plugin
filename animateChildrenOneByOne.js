8bf7aa2c7d731ebb21b6abe0ff606332411c32b2
Paulina  Kosińska <paulinakosinska95@gmail.com>
2018-04-09 22:17:21 +0200

    clean up

--- animateChildrenOneByOne.js
+++ animateChildrenOneByOne.js
@@ -2,20 +2,19 @@ $.fn.animateChildrenOneByOne = function (animate, delay, duration) {
 	delay = (delay !== undefined) ? delay : 2000;
 	duration = (duration !== undefined) ? duration : 500;
 
-	this.each(function() {
+	this.each(function () {
 		var i = 0;
 		$(this).children().each(function () {
 			var currentElement = $(this);
 			i++;
-			setTimeout(function() {
+			setTimeout(function () {
 				try {
 					currentElement[animate](duration);
-				}
-				catch (err)	{
+				} catch (err) {
 					console.error(`Invalid animation [${err}]`);
 				}
 			}, i * delay);
 		});
 	})
-	
+
 };
