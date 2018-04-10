5ddb33f250a19742b5a83c594194ab777cb2a57f
Paulina  Kosińska <paulinakosinska95@gmail.com>
2018-04-10 10:17:55 +0200

    add demo

---
+++ demo/script.js
@@ -0,0 +1,15 @@
+$("#cards-fade-out-btn").click(function () {
+    $("#cards-fade-out").animateChildrenOneByOne("fadeOut");
+});
+$("#cards-fade-in-btn").click(function () {
+    $("#cards-fade-in").animateChildrenOneByOne("fadeIn");
+});
+$("#cards-slide-down-btn").click(function () {
+    $("#cards-slide-down").animateChildrenOneByOne("slideDown");
+});
+$("#cards-slide-up-btn").click(function () {
+    $("#cards-slide-up").animateChildrenOneByOne("slideUp");
+});
+$("#ul-fade-out-btn").click(function () {
+    $("#ul-fade-out").animateChildrenOneByOne("fadeOut");
+});
